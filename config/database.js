0bfed9dc20ddf09414f6d881ea74e22797b5d60b
zeedtanue <zeedtanue@gmail.com>
2020-10-23 11:58:10 +0800

    Update database config

--- config/database.js
+++ config/database.js
@@ -4,11 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: 'mongodb://dev:quiziappanddoctorwho@clusterinitial-shard-00-00.qggyk.mongodb.net:27017,clusterinitial-shard-00-01.qggyk.mongodb.net:27017,clusterinitial-shard-00-02.qggyk.mongodb.net:27017/quiz-backend?ssl=true&replicaSet=atlas-mdl8d3-shard-0&authSource=admin&retryWrites=true&w=majority'
+        uri: '${process.env.DATABASE_URI}',
+        database: "${process.env.DATABASE_NAME}"
       },
       options: {
 
-        ssl: env.bool( false),
+        ssl: true,
       },
     },
   },
