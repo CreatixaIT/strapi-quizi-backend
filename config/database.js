0e19e4c4eb478fe6c1ba94f64b463789ff5ce5c3
Creatixa <64127769+CreatixaIT@users.noreply.github.com>
2021-05-25 13:40:16 +0800

    Update database url

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: { 
-        uri:'mongodb+srv://dev_user:yhCneJWyW7fP4SAF@clusterinitial-shard-00-00.qggyk.mongodb.net:27017,clusterinitial-shard-00-01.qggyk.mongodb.net:27017,clusterinitial-shard-00-02.qggyk.mongodb.net:27017/quiz-backend?ssl=true&replicaSet=atlas-mdl8d3-shard-0&authSource=admin&retryWrites=true&tlsInsecure=true&w=majority'
+        uri:'mongodb://dev_user:yhCneJWyW7fP4SAF@clusterinitial-shard-00-00.qggyk.mongodb.net:27017,clusterinitial-shard-00-01.qggyk.mongodb.net:27017,clusterinitial-shard-00-02.qggyk.mongodb.net:27017/quiz-backend?ssl=true&replicaSet=atlas-mdl8d3-shard-0&authSource=admin&retryWrites=true&tlsInsecure=true&w=majority'
       },
       options: {
         ssl: true,
